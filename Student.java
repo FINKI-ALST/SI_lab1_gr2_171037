886c499ded5a8071f70cfac12816543ad3736ea4
FINKI-ALST <aleksandra.stefanovska@students.finki.ukim.mk>
2020-04-07 20:10:07 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -14,7 +14,56 @@ class Student {
 		}
 
 
+		public Student(String index) {
+			this.index = index;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public void setIndex(String index) {
+			this.index = index;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public String getIndex() {
+			return index;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public double getAverage() {
+			double suma = 0;
+			SLLNode<Integer> tmp = grades.getFirst();
+			while(tmp != null){
+				suma = suma + tmp.element;
+				tmp = tmp.succ; }
+			double prosek = suma/grades.length();
+		}
+
+		public int ECTSCredits() {
+			suma = 0;
+			SLLNode<Integer> tmp = grades.getFirst();
+			if (tmp.element = "6" || tmp.element = "7" || tmp.element = "8" || tmp.element = "9" || tmp.element = "10"){
+				suma += 6;
+			}
+			tmp=tmp.succ;
+		}
+
+
 	}
+
+
 }
 
 
